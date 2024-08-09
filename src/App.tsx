cd73a055bf863ba97c0cd3242080ebad1bb7fede
Amer Mufti <amufti111@gmail.com>
2024-08-09 12:30:00 -0700

    Err fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 //import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
+//import type { Schema } from "../amplify/data/resource";
 //import { generateClient } from "aws-amplify/data";
 import { Card, Flex, Image, Badge, Text, StepperField, Button } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
