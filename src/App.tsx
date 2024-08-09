feb85e21c6f86de1cf623960a4f6dd5d3f2bc7c9
Amer Mufti <amufti111@gmail.com>
2024-08-09 12:17:07 -0700

    Error fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { Card, Flex, Image, Badge, Text, StepperField, Button } from "@aws-amplify/ui-react";
@@ -9,11 +9,11 @@ const client = generateClient<Schema>();
 function App() {
   //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+  //useEffect(() => {
+  //  client.models.Todo.observeQuery().subscribe({
+  //    next: (data) => setTodos([...data.items]),
+  //  });
+  //}, []);
 
   //function createTodo() {
   //  client.models.Todo.create({ content: window.prompt("Todo content"), isDone: true });
