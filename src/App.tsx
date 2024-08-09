080d291a8fad71b194406dd39a058ee830ab6d21
Amer Mufti <amufti111@gmail.com>
2024-08-09 12:05:50 -0700

    Removed initial sample code

--- src/App.tsx
+++ src/App.tsx
@@ -21,21 +21,6 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-
       <Card variation="elevated">
         <Flex alignItems="flex-start">
           <Image src="/amplify-placeholder.svg"
@@ -74,3 +59,21 @@ function App() {
 }
 
 export default App;
+
+/*
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
+
+*/
