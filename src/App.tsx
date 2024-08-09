d5765b421d86c2e9304015931ecce89cb48d8013
Amer Mufti <amufti111@gmail.com>
2024-08-09 13:38:02 -0700

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,12 @@ function App() {
   return (
     <main>
 
-      <Card variation="elevated">
+      <Card 
+        variation="elevated" 
+        width="80em"
+        marginTop="0.3em"
+        marginBottom="0.3em"
+    >
         <TextField
           placeholder=""
           label="Requestor Name"
@@ -50,8 +55,6 @@ function App() {
         <RadioGroupField
           legend="Severity"
           name="Severity"
-          marginTop="0.3em"
-          marginBottom="0.3em"
           defaultValue="Normal"
           direction="row"
         >
@@ -64,16 +67,12 @@ function App() {
           placeholder=""
           rows={4} />
         <Text
-          marginTop="0.3em"
-          marginBottom="0.3em"
         >
           Requested: 2024-08-09 12:30PM PDT
         </Text>
         <RadioGroupField
           legend="Status"
           name="Status"
-          marginTop="0.3em"
-          marginBottom="0.3em"
           defaultValue="Submitted"
           direction="row"
           isDisabled
