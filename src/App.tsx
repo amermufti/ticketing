754de408a7335303f487813046af53e111ed8b32
Amer Mufti <amufti111@gmail.com>
2024-08-09 12:22:07 -0700

    u

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 //import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+//import { generateClient } from "aws-amplify/data";
 import { Card, Flex, Image, Badge, Text, StepperField, Button } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
 
-const client = generateClient<Schema>();
+//const client = generateClient<Schema>();
 
 function App() {
   //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
