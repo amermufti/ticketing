cb327936135f4066983e055a82ad4616562589de
Amer Mufti <amufti111@gmail.com>
2024-08-09 13:50:48 -0700

    Table added

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,51 @@ import {
   RadioGroupField,
   Radio,
   Card, 
+  Text, 
   //Flex, 
   //Image, 
   //Badge, 
-  Text, 
   //StepperField, 
   //Button 
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  ThemeProvider,
+  Theme,  
 } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
 
 //const client = generateClient<Schema>();
+
+const theme: Theme = {
+  name: 'table-theme',
+  tokens: {
+    components: {
+      table: {
+        row: {
+          hover: {
+            backgroundColor: { value: '{colors.blue.20}' },
+          },
+
+          striped: {
+            backgroundColor: { value: '{colors.blue.10}' },
+          },
+        },
+
+        header: {
+          color: { value: '{colors.blue.80}' },
+          fontSize: { value: '{fontSizes.xl}' },
+        },
+
+        data: {
+          fontWeight: { value: '{fontWeights.semibold}' },
+        },
+      },
+    },
+  },
+};
 
 function App() {
   /*
@@ -36,8 +71,8 @@ function App() {
   return (
     <main>
 
-      <Card 
-        variation="elevated" 
+      <Card
+        variation="elevated"
         width="60em"
         marginTop="0.3em"
         marginBottom="0.3em"
@@ -46,23 +81,23 @@ function App() {
           placeholder=""
           label="Requestor Name:"
           errorMessage="There is an error"
-          marginTop="0.4em"
-          marginBottom="0.4em"
+          marginTop="0.5em"
+          marginBottom="0.5em"
         />
         <TextField
           placeholder=""
           label="Requestor Email:"
           errorMessage="There is an error"
-          marginTop="0.4em"
-          marginBottom="0.4em"
+          marginTop="0.5em"
+          marginBottom="0.5em"
         />
         <RadioGroupField
           legend="Severity: "
           name="Severity"
           defaultValue="Normal"
           direction="row"
-          marginTop="0.4em"
-          marginBottom="0.4em"
+          marginTop="0.5em"
+          marginBottom="0.5em"
         >
           <Radio value="Normal">Normal</Radio>
           <Radio value="High">High</Radio>
@@ -71,13 +106,13 @@ function App() {
           label="Please describe the issue:"
           name="notes_request"
           placeholder=""
-          rows={4} 
-          marginTop="0.4em"
-          marginBottom="0.4em"
+          rows={4}
+          marginTop="0.5em"
+          marginBottom="0.5em"
         />
         <Text
-          marginTop="0.4em"
-          marginBottom="0.4em"
+          marginTop="0.5em"
+          marginBottom="0.5em"
         >
           Requested: 2024-08-09 12:30PM PDT
         </Text>
@@ -86,8 +121,8 @@ function App() {
           name="Status"
           defaultValue="Submitted"
           direction="row"
-          marginTop="0.4em"
-          marginBottom="0.4em"
+          marginTop="0.5em"
+          marginBottom="0.5em"
           isDisabled
         >
           <Radio value="Submitted">Submitted</Radio>
@@ -98,17 +133,46 @@ function App() {
           label="Resolution:"
           name="notes_resolution"
           placeholder=""
-          rows={4} 
-          marginTop="0.4em"
-          marginBottom="0.4em"
+          rows={4}
+          marginTop="0.5em"
+          marginBottom="0.5em"
         />
         <Text
-          marginTop="0.4em"
-          marginBottom="0.4em"
+          marginTop="0.5em"
+          marginBottom="0.5em"
         >
           Resolved: 2024-08-09 03:30PM PDT
         </Text>
       </Card>
+
+      <ThemeProvider theme={theme} colorMode="light">
+        <Table highlightOnHover variation="striped">
+          <TableHead>
+            <TableRow>
+              <TableCell as="th">Citrus</TableCell>
+              <TableCell as="th">Stone Fruit</TableCell>
+              <TableCell as="th">Berry</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Orange</TableCell>
+              <TableCell>Nectarine</TableCell>
+              <TableCell>Raspberry</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Grapefruit</TableCell>
+              <TableCell>Apricot</TableCell>
+              <TableCell>Blueberry</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Lime</TableCell>
+              <TableCell>Peach</TableCell>
+              <TableCell>Strawberry</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </ThemeProvider>
     </main>
   );
 }
