ae0bc9da138a4c4cf9c37e4caf1d8cea094caeb1
Amer Mufti <amufti111@gmail.com>
2024-08-21 21:31:25 -0700

    Fix

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -25,12 +25,14 @@ const schema = a.schema({
     .model({
       requester_name: a.string(),
       requester_email: a.string(),
+      /*
       severity: a.string(),
       reason_for_high: a.string(),
       notes_request: a.string(),
       time_requested: a.string(),
       status: a.string(),
       notes_resolution: a.string(),
+      */
       time_resolved: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
